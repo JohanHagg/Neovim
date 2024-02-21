a51259a3cb7623f304564515aab6df97d950c810
Johan Häggström <johan.haggstroem@gmail.com>
2024-02-21 15:06:02 +0100

    Add catppuccin

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,6 +1,7 @@
 vim.cmd("set expandtab")
 vim.cmd("set clipboard=unnamedplus")
 
+-- Lazy
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
   vim.fn.system({
@@ -14,8 +15,13 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-local plugins = {}
+local plugins = {
+  { "catppuccin/nvim", name = "catppuccin", priority = 1000 }
+}
 local opts = {}
 
 require("lazy").setup(plugins, opts)
 
+
+require("catppuccin").setup()
+vim.cmd.colorscheme "catppuccin"
