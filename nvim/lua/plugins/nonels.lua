deb22708a25852545bab2b5fb3067da35221ad2c
Johan Häggström <johan.haggstroem@gmail.com>
2024-02-28 21:50:19 +0100

    Add format on save

--- nvim/lua/plugins/nonels.lua
+++ nvim/lua/plugins/nonels.lua
@@ -1,14 +1,23 @@
 return {
-	"nvimtools/none-ls.nvim",
-	config = function()
-		local null_ls = require("null-ls")
-		null_ls.setup({
-			sources = {
-				null_ls.builtins.formatting.stylua,
-				null_ls.builtins.formatting.prettier,
-			},
-		})
+    "nvimtools/none-ls.nvim",
+    config = function()
+        local null_ls = require("null-ls")
+        null_ls.setup({
+            sources = {
+                null_ls.builtins.formatting.stylua,
+                null_ls.builtins.formatting.prettier,
+            },
+        })
 
-		vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
-	end,
+        -- Format manually
+        vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
+
+        -- Format before save
+        vim.keymap.set("n", "<leader>w", function()
+            vim.lsp.buf.format()
+            vim.cmd(":wa")
+        end)
+
+        -- Define a function to format the buffer and save the file
+    end,
 }
