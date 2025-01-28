354f1b050331abe8dfff0bf31a82829ea885dbc7
Johan Häggström <johan.haggstroem@gmail.com>
2025-01-28 22:03:31 +0100

    Improve gitsigns integration

--- nvim/lua/plugins/gitsigns.lua
+++ nvim/lua/plugins/gitsigns.lua
@@ -2,8 +2,46 @@ return {
   "lewis6991/gitsigns.nvim",
   config = function()
     require("gitsigns").setup({
-      -- (Currently disabled) To enable git blame inline
-      current_line_blame = false,
+      on_attach = function(bufnr)
+        local gitsigns = require("gitsigns")
+
+        -- Local helper that I don't know what it's for
+        local function map(mode, l, r, opts)
+          opts = opts or {}
+          opts.buffer = bufnr
+          vim.keymap.set(mode, l, r, opts)
+        end
+
+        -- TODO: Figure out if this is useful
+        -- map("n", "<leader>ts", gitsigns.stage_hunk)
+        map("n", "<leader>tu", gitsigns.reset_hunk)
+
+        -- TODO: Find better key binding to use Ctrl
+        map("n", "<leader>tn", gitsigns.next_hunk)
+
+        map("n", "[c", function()
+          if vim.wo.diff then
+            vim.cmd.normal({ "[c", bang = true })
+          else
+            gitsigns.nav_hunk("prev")
+          end
+        end)
+
+        -- Preview hunk as popout modal
+        map("n", "<leader>tp", gitsigns.preview_hunk)
+        -- Preview hunk inline
+        map("n", "<leader>ti", gitsigns.preview_hunk_inline)
+
+        -- Toggle git blame
+        map("n", "<leader>tb", function()
+          gitsigns.blame_line({ full = true })
+        end)
+
+        -- Toggle deleted content in line
+        map("n", "<leader>td", gitsigns.toggle_deleted)
+        -- Toggle line differences in line
+        map("n", "<leader>tw", gitsigns.toggle_word_diff)
+      end,
     })
   end,
 }
